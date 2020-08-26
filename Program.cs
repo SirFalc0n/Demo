429e3055648a5654a9ca2dd8a89caa724025a5c2
SirFalc0n <70292868+SirFalc0n@users.noreply.github.com>
2020-08-26 16:56:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String var1 = Console.ReadLine();
+            Console.WriteLine("Hullo: " + var1);
+        }
+    }
+}
